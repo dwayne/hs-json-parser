c96129f1469e7567c8b97d06fd266abd9a3bee2d
Dwayne Crooks <me@dwaynecrooks.com>
2026-05-07 06:55:42 -0400

    Implement signedNatural

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,20 +1,78 @@
-module Lexer (Sign(..), sign, ws) where
+module Lexer
+  ( Parser, Error
+  , SignedNatural(..), Sign(..), signedNatural
+  , sign
+  , ws
+  ) where
+
+import qualified Data.Char as Char
+import qualified Data.Text as T
 
 import Control.Applicative ((<|>))
 import Control.Monad (void)
 import Data.Text (Text)
 import Data.Void (Void)
-import Text.Megaparsec ((<?>), Parsec, takeWhileP)
+import Numeric.Natural (Natural)
+import Text.Megaparsec ((<?>), Parsec, ParseErrorBundle, satisfy, takeWhileP)
 import Text.Megaparsec.Char (char)
 
 
 type Parser = Parsec Void Text
+type Error = ParseErrorBundle Text Void
+
+
+data SignedNatural = SignedNatural Sign Natural
+  deriving (Eq, Show)
 
 
 data Sign
   = Plus
   | Minus
   deriving (Eq, Show)
+
+
+signedNatural :: Parser SignedNatural
+signedNatural =
+  --
+  -- integer
+  --     digit
+  --     onenine digits
+  --     '-' digit
+  --     '-' onenine digits
+  --
+  -- digits
+  --     digit
+  --     digit digits
+  --
+  -- digit
+  --     '0'
+  --     onenine
+  --
+  -- onenine
+  --     '1' . '9'
+  --
+  SignedNatural <$> sign <*> (zero <|> positiveNumber)
+  where
+    sign :: Parser Sign
+    sign = Minus <$ char '-' <|> pure Plus <?> "sign"
+
+    zero :: Parser Natural
+    zero = 0 <$ char '0' <?> "zero"
+
+    positiveNumber :: Parser Natural
+    positiveNumber = toNatural <$> oneNine <*> digits <?> "positive number"
+
+    toNatural :: Char -> Text -> Natural
+    toNatural ch = read . T.unpack . T.cons ch
+
+    oneNine :: Parser Char
+    oneNine = satisfy isOneNine <?> "non-zero digit"
+      where
+        isOneNine ch = ch /= '0' && Char.isDigit ch
+
+    digits :: Parser Text
+    digits = takeWhileP (Just "digit") Char.isDigit
+
 
 
 sign :: Parser (Maybe Sign)
--- tests/Test/LexerSpec.hs
+++ tests/Test/LexerSpec.hs
@@ -4,9 +4,10 @@ module Test.LexerSpec (spec) where
 
 import qualified Lexer as L
 
+import Data.Text (Text)
 import Test.Hspec
 import Test.Hspec.Megaparsec
-import Text.Megaparsec (parse)
+import Text.Megaparsec (parse, eof)
 
 
 spec :: Spec
@@ -14,6 +15,7 @@ spec =
   describe "Lexer" $ do
     wsSpec
     signSpec
+    signedNaturalSpec
 
 
 wsSpec :: Spec
@@ -21,16 +23,16 @@ wsSpec =
   describe "ws" $ do
     it "parses zero or more whitespace characters" $ do
       -- zero
-      parse L.ws "" `shouldSucceedOn` ""
+      parseTillEnd L.ws `shouldSucceedOn` ""
 
       -- one
-      parse L.ws "" `shouldSucceedOn` " "
-      parse L.ws "" `shouldSucceedOn` "\n"
-      parse L.ws "" `shouldSucceedOn` "\r"
-      parse L.ws "" `shouldSucceedOn` "\t"
+      parseTillEnd L.ws `shouldSucceedOn` " "
+      parseTillEnd L.ws `shouldSucceedOn` "\n"
+      parseTillEnd L.ws `shouldSucceedOn` "\r"
+      parseTillEnd L.ws `shouldSucceedOn` "\t"
 
       -- more than one
-      parse L.ws "" `shouldSucceedOn` " \n\r\t\t\r\n "
+      parseTillEnd L.ws `shouldSucceedOn` " \n\r\t\t\r\n "
 
 
 signSpec :: Spec
@@ -38,12 +40,54 @@ signSpec =
   describe "sign" $ do
     it "parses an optional plus or minus sign" $ do
       -- plus sign
-      parse L.sign "" "+" `shouldParse` Just L.Plus
+      parseTillEnd L.sign "+" `shouldParse` Just L.Plus
 
       -- minus sign
-      parse L.sign "" "-" `shouldParse` Just L.Minus
+      parseTillEnd L.sign "-" `shouldParse` Just L.Minus
 
       -- no sign
-      parse L.sign "" "" `shouldParse` Nothing
-      parse L.sign "" "a" `shouldParse` Nothing
-      parse L.sign "" "1" `shouldParse` Nothing
+      parseTillEnd L.sign "" `shouldParse` Nothing
+
+      -- expected failures
+      parseTillEnd L.sign `shouldFailOn` "1"
+
+
+signedNaturalSpec :: Spec
+signedNaturalSpec =
+  describe "signedNatural" $ do
+    it "parses a signed natural number" $ do
+      -- zero
+      parseTillEnd L.signedNatural "0" `shouldParse` L.SignedNatural L.Plus 0
+      parseTillEnd L.signedNatural "-0" `shouldParse` L.SignedNatural L.Minus 0
+
+      -- non-zero digit
+      parseTillEnd L.signedNatural "1" `shouldParse` L.SignedNatural L.Plus 1
+      parseTillEnd L.signedNatural "2" `shouldParse` L.SignedNatural L.Plus 2
+      parseTillEnd L.signedNatural "3" `shouldParse` L.SignedNatural L.Plus 3
+      parseTillEnd L.signedNatural "4" `shouldParse` L.SignedNatural L.Plus 4
+      parseTillEnd L.signedNatural "5" `shouldParse` L.SignedNatural L.Plus 5
+      parseTillEnd L.signedNatural "6" `shouldParse` L.SignedNatural L.Plus 6
+      parseTillEnd L.signedNatural "7" `shouldParse` L.SignedNatural L.Plus 7
+      parseTillEnd L.signedNatural "8" `shouldParse` L.SignedNatural L.Plus 8
+      parseTillEnd L.signedNatural "9" `shouldParse` L.SignedNatural L.Plus 9
+
+      -- more than one digit
+      parseTillEnd L.signedNatural "10" `shouldParse` L.SignedNatural L.Plus 10
+      parseTillEnd L.signedNatural "1234567890" `shouldParse` L.SignedNatural L.Plus 1234567890
+
+      -- negative numbers
+      parseTillEnd L.signedNatural "-1" `shouldParse` L.SignedNatural L.Minus 1
+      parseTillEnd L.signedNatural "-10" `shouldParse` L.SignedNatural L.Minus 10
+
+      -- expected failures
+      parseTillEnd L.signedNatural `shouldFailOn` ""
+      parseTillEnd L.signedNatural `shouldFailOn` "-"
+      parseTillEnd L.signedNatural `shouldFailOn` "01"
+
+
+-- Helpers
+
+
+parseTillEnd :: L.Parser a -> Text -> Either L.Error a
+parseTillEnd p =
+  parse (p <* eof) ""
