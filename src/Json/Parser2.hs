2e2b023c46773baaf773466cf027fd1f311e4401
Dwayne Crooks <me@dwaynecrooks.com>
2026-05-13 07:04:29 -0400

    Add number

--- src/Json/Parser2.hs
+++ src/Json/Parser2.hs
@@ -3,25 +3,110 @@
 module Json.Parser2
     ( Parser, Error
     , false, true, null
+    , Number(Number), Sign(..), number
     , ws1
     ) where
 
+import qualified Data.Char as Char
+import qualified Data.Text as T
 import qualified Text.Megaparsec.Char as Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
-import Control.Applicative (empty)
+import Control.Applicative ((<|>), empty)
 import Control.Monad (void)
 import Data.Text (Text)
 import Data.Void (Void)
 import Prelude hiding (null)
-import Text.Megaparsec (Parsec, ParseErrorBundle, notFollowedBy, takeWhile1P)
-import Text.Megaparsec.Char (alphaNumChar)
+import Text.Megaparsec (Parsec, ParseErrorBundle, notFollowedBy, satisfy, takeWhileP, takeWhile1P)
+import Text.Megaparsec.Char (alphaNumChar, char)
 
 
 type Parser = Parsec Void Text
 
 
 type Error = ParseErrorBundle Text Void
+
+
+-- Numbers
+
+
+data Number
+  = Number
+      { numSign :: Sign
+      , numDigits :: Text
+      , numFraction :: Maybe Text
+      , numExponent :: Maybe (Sign, Text)
+      }
+  deriving (Eq, Show)
+
+
+data Sign
+  = Plus
+  | Minus
+  deriving (Eq, Show)
+
+
+number :: Parser Number
+number =
+  --
+  -- number = [ minus ] int [ frac ] [ exp ]
+  --
+  -- minus = %x2D         ; -
+  --
+  -- int      = zero / ( digit1-9 *DIGIT )
+  -- zero     = %x30      ; 0
+  -- digit1-9 = %x31-39   ; 1-9
+  --
+  -- frac          = decimal-point 1*DIGIT
+  -- decimal-point = %x2E ; .
+  --
+  -- exp  = e [ minus / plus ] 1*DIGIT
+  -- e    = %x65 / %x45   ; e E
+  -- plus = %x2B          ; +
+  --
+  Number <$> leadingSign <*> naturalNumber <*> optional fractionalPart <*> optional exponentPart
+  where
+    leadingSign :: Parser Sign
+    leadingSign = minus <|> pure Plus
+
+    naturalNumber :: Parser Text
+    naturalNumber = zero <|> positiveNumber
+
+    zero :: Parser Text
+    zero = Char.string "0"
+
+    positiveNumber :: Parser Text
+    positiveNumber = T.cons <$> oneToNine <*> zeroOrMoreDigits
+
+    oneToNine :: Parser Char
+    oneToNine = satisfy isNonZeroDigit
+
+    isNonZeroDigit :: Char -> Bool
+    isNonZeroDigit ch = Char.isDigit ch && ch /= '0'
+
+    zeroOrMoreDigits :: Parser Text
+    zeroOrMoreDigits = takeWhileP (Just "digit") Char.isDigit
+
+    fractionalPart :: Parser Text
+    fractionalPart = char '.' *> oneOrMoreDigits
+
+    exponentPart :: Parser (Sign, Text)
+    exponentPart = (,) <$ e <*> exponentSign <*> oneOrMoreDigits
+
+    e :: Parser Char
+    e = char 'E' <|> char 'e'
+
+    exponentSign :: Parser Sign
+    exponentSign = minus <|> plus <|> pure Plus
+
+    minus :: Parser Sign
+    minus = Minus <$ char '-'
+
+    plus :: Parser Sign
+    plus = Plus <$ char '+'
+
+    oneOrMoreDigits :: Parser Text
+    oneOrMoreDigits = takeWhile1P (Just "digit") Char.isDigit
 
 
 -- Literals
@@ -109,3 +194,10 @@ ws1 =
       || ch == '\x09'
       || ch == '\x0A'
       || ch == '\x0D'
+
+
+-- Helpers
+
+
+optional :: Parser a -> Parser (Maybe a)
+optional p = (Just <$> p) <|> pure Nothing
--- tests/Test/Json/Parser2Spec.hs
+++ tests/Test/Json/Parser2Spec.hs
@@ -17,6 +17,7 @@ spec =
     falseSpec
     trueSpec
     nullSpec
+    numberSpec
 
 
 wsSpec :: Spec
@@ -72,6 +73,62 @@ nullSpec =
       parseTillEnd P.null `shouldFailOn` "NULL"
       parseTillEnd P.null `shouldFailOn` "Null"
       parseTillEnd P.null `shouldFailOn` "nullish"
+
+
+numberSpec :: Spec
+numberSpec =
+  describe "number" $ do
+    it "zero" $ do
+      parseTillEnd P.number "0" `shouldParse` P.Number P.Plus "0" Nothing Nothing
+
+    it "negative zero" $ do
+      parseTillEnd P.number "-0" `shouldParse` P.Number P.Minus "0" Nothing Nothing
+
+    it "positive integer" $ do
+      parseTillEnd P.number "1" `shouldParse` P.Number P.Plus "1" Nothing Nothing
+      parseTillEnd P.number "2" `shouldParse` P.Number P.Plus "2" Nothing Nothing
+      parseTillEnd P.number "3" `shouldParse` P.Number P.Plus "3" Nothing Nothing
+      parseTillEnd P.number "4" `shouldParse` P.Number P.Plus "4" Nothing Nothing
+      parseTillEnd P.number "5" `shouldParse` P.Number P.Plus "5" Nothing Nothing
+      parseTillEnd P.number "6" `shouldParse` P.Number P.Plus "6" Nothing Nothing
+      parseTillEnd P.number "7" `shouldParse` P.Number P.Plus "7" Nothing Nothing
+      parseTillEnd P.number "8" `shouldParse` P.Number P.Plus "8" Nothing Nothing
+      parseTillEnd P.number "9" `shouldParse` P.Number P.Plus "9" Nothing Nothing
+      parseTillEnd P.number "123456789" `shouldParse` P.Number P.Plus "123456789" Nothing Nothing
+
+    it "negative integer" $ do
+      parseTillEnd P.number "-1" `shouldParse` P.Number P.Minus "1" Nothing Nothing
+      parseTillEnd P.number "-2" `shouldParse` P.Number P.Minus "2" Nothing Nothing
+      parseTillEnd P.number "-3" `shouldParse` P.Number P.Minus "3" Nothing Nothing
+      parseTillEnd P.number "-4" `shouldParse` P.Number P.Minus "4" Nothing Nothing
+      parseTillEnd P.number "-5" `shouldParse` P.Number P.Minus "5" Nothing Nothing
+      parseTillEnd P.number "-6" `shouldParse` P.Number P.Minus "6" Nothing Nothing
+      parseTillEnd P.number "-7" `shouldParse` P.Number P.Minus "7" Nothing Nothing
+      parseTillEnd P.number "-8" `shouldParse` P.Number P.Minus "8" Nothing Nothing
+      parseTillEnd P.number "-9" `shouldParse` P.Number P.Minus "9" Nothing Nothing
+      parseTillEnd P.number "-123456789" `shouldParse` P.Number P.Minus "123456789" Nothing Nothing
+
+    it "fractional part" $ do
+      parseTillEnd P.number "123.456" `shouldParse` P.Number P.Plus "123" (Just "456") Nothing
+      parseTillEnd P.number "-123.456" `shouldParse` P.Number P.Minus "123" (Just "456") Nothing
+      parseTillEnd P.number "0.5" `shouldParse` P.Number P.Plus "0" (Just "5") Nothing
+      parseTillEnd P.number "-0.5" `shouldParse` P.Number P.Minus "0" (Just "5") Nothing
+      parseTillEnd P.number "0.005" `shouldParse` P.Number P.Plus "0" (Just "005") Nothing
+      parseTillEnd P.number "0.000" `shouldParse` P.Number P.Plus "0" (Just "000") Nothing
+      parseTillEnd P.number "-0.000" `shouldParse` P.Number P.Minus "0" (Just "000") Nothing
+
+    it "exponent part" $ do
+      parseTillEnd P.number "123.456E78" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Plus, "78"))
+      parseTillEnd P.number "123.456E+78" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Plus, "78"))
+      parseTillEnd P.number "123.456E-78" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Minus, "78"))
+
+      parseTillEnd P.number "123.456e78" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Plus, "78"))
+      parseTillEnd P.number "123.456e+78" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Plus, "78"))
+      parseTillEnd P.number "123.456e-78" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Minus, "78"))
+
+      parseTillEnd P.number "123.456E0" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Plus, "0"))
+      parseTillEnd P.number "123.456E00" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Plus, "00"))
+      parseTillEnd P.number "123.456E-0" `shouldParse` P.Number P.Plus "123" (Just "456") (Just (P.Minus, "0"))
 
 
 -- Helpers
