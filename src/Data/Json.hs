4b5aa3c3d4b2c5317af76cf8d634580e951fa379
Dwayne Crooks <me@dwaynecrooks.com>
2026-05-24 10:22:30 -0400

    Add new data structures to the public API to keep the internal data structures hidden

--- src/Data/Json.hs
+++ src/Data/Json.hs
@@ -1,10 +1,74 @@
-module Data.Json (parse) where
+module Data.Json
+  ( Json(..)
+  , Number(..), Sign(..)
+  , Array
+  , Object
+  , SyntaxError
+  , parse
+  ) where
 
 import qualified Data.Json.Internal.Parser as P
 import qualified Text.Megaparsec as Megaparsec
 
+import Data.Bifunctor (first, second)
 import Data.Text (Text)
+import Data.Void (Void)
+import Text.Megaparsec (ParseErrorBundle)
 
 
-parse :: Text -> Either P.Error P.Json
-parse = Megaparsec.parse P.json ""
+data Json
+  = JsonNull
+  | JsonBoolean Bool
+  | JsonNumber Number
+  | JsonString Text
+  | JsonArray Array
+  | JsonObject Object
+  deriving (Eq, Show)
+
+
+data Number
+  = Number
+      { numSign :: Sign
+      , numDigits :: Text
+      , numFraction :: Maybe Text
+      , numExponent :: Maybe (Sign, Text)
+      }
+  deriving (Eq, Show)
+
+
+data Sign
+  = Plus
+  | Minus
+  deriving (Eq, Show)
+
+
+type Array = [Json]
+
+
+type Object = [(Text, Json)]
+
+
+type SyntaxError = ParseErrorBundle Text Void
+
+
+parse :: Text -> Either SyntaxError Json
+parse = fmap convertJson . Megaparsec.parse P.json ""
+
+
+convertJson :: P.Json -> Json
+convertJson P.JsonNull        = JsonNull
+convertJson (P.JsonBoolean b) = JsonBoolean b
+convertJson (P.JsonNumber n)  = JsonNumber $ convertNumber n
+convertJson (P.JsonString t)  = JsonString t
+convertJson (P.JsonArray a)   = JsonArray $ map convertJson a
+convertJson (P.JsonObject o)  = JsonObject $ map (second convertJson) o
+
+
+convertNumber :: P.Number -> Number
+convertNumber (P.Number s d f e) =
+  Number (convertSign s) d f (fmap (first convertSign) e)
+
+
+convertSign :: P.Sign -> Sign
+convertSign P.Plus  = Plus
+convertSign P.Minus = Minus
